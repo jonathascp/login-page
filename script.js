472b905050ae40300bcae3440a35c72d5096c468
Jonathas dos Anjos <127884309+jonathascp@users.noreply.github.com>
2023-08-08 13:21:56 -0300

    Mostrar e esconder senha

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const btnEnviar = document.querySelector('.enviar');
+const senha_icone = document.getElementById('icone_senha');
+const senha = document.getElementById('isenha');
+
+
 btnEnviar.addEventListener('click', () => alert('Login efetuado com sucesso.'));
+senha_icone.addEventListener('click',showAndHide);
+function showAndHide()
+{
+    if(senha.type == 'password')
+    {
+        senha.setAttribute('type','text');
+        icone_senha.setAttribute('src','imagens/hide.png');
+    }
+    else
+    {
+        senha.setAttribute('type','password');
+        icone_senha.setAttribute('src','imagens/show.png');
+    }
+}
